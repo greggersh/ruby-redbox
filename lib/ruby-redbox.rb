191739634a32b4ee9be0b2c0fe336b2a5abffa00
Greg Gershman <greggersh@yahoo.com>
2010-12-31 14:47:35 -0500

    Enough with httparty, it aint workin

--- lib/ruby-redbox.rb
+++ lib/ruby-redbox.rb
@@ -1,11 +1,6 @@
 require 'rubygems'
-gem 'httparty'
-require 'httparty'
 
 class RedboxAPI
-  include HTTParty
-  base_uri "www.redbox.com"
-  
   class << self
   
     def get_titles
@@ -21,8 +16,14 @@ class RedboxAPI
     
     def find_nearest_kiosks(title_id, zip)
       body = "{\"filters\":{\"proximity\":{\"lat\":39.3668174,\"lng\":-76.670948,\"radius\":50}},\"resultOptions\":{\"max\":50,\"profile\":true,\"status\":true,\"proximity\":true,\"user\":true,\"inventory\":true,\"inventoryProducts\":[\"#{title_id}\"]}}"
-      headers = {"Host" => "www.redbox.com", "Accept" => "application/json, text/javascript, */*", "X-Requested-With" => "XMLHttpRequest", "User-Agent" => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13", "Cookie" => "rbuser=Validation=5u/UfKCJpwFUJOW0wYNsZrUJ/1Wc/yOL4HF3zhsxTC8=; RB_2.0=1; _json=%7B%22isBlurayWarningEnabled%22%3A%22true%22%2C%22isGamesWarningEnabled%22%3A%22true%22%2C%22locationSearchValue%22%3A%22%5C%2221209%5C%22%22%7D; s_vi=[CS]v1|268A179C851D2012-6000010160000D1E[CE]; ASP.NET_SessionId=oszj4v53yetgxjiiouic4cg4; LB=1518497546.0.0000; s_cc=true; c_m=undefinedDirect%20LoadDirect%20Load; s_sq=%5B%5BB%5D%5D", "__K" => "uFyVRI4STRLYVcsJY2zC92etFBe6N/ef19yTBpaRipY="} 
-      self.post("/api/Store/GetStores/", :body => body.to_json, :headers => headers)
+      headers = {"Host" => "www.redbox.com", "Accept" => "application/json, text/javascript, */*", "X-Requested-With" => "XMLHttpRequest", "User-Agent" => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13", "Cookie" => "rbuser=Validation=5u/UfKCJpwFUJOW0wYNsZrUJ/1Wc/yOL4HF3zhsxTC8=; RB_2.0=1; _json=%7B%22isBlurayWarningEnabled%22%3A%22true%22%2C%22isGamesWarningEnabled%22%3A%22true%22%2C%22locationSearchValue%22%3A%22%5C%2221209%5C%22%22%7D; s_vi=[CS]v1|268A179C851D2012-6000010160000D1E[CE]; ASP.NET_SessionId=oszj4v53yetgxjiiouic4cg4; LB=1518497546.0.0000; s_cc=true; c_m=undefinedDirect%20LoadDirect%20Load; s_sq=%5B%5BB%5D%5D", "__K" => "uFyVRI4STRLYVcsJY2zC92etFBe6N/ef19yTBpaRipY="}
+      url = URI.parse("http://www.redbox.com/api/Store/GetStores/")
+      request = Net::HTTP::Post.new(url.path)
+      request.body = body
+      headers.each do |key, value|
+        request[key] = value
+      end
+      Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
     end
   end
 end
