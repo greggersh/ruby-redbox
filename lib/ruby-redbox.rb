8e408d6297546a64f3f56800bef5e023768946b7
Greg Gershman <greggersh@yahoo.com>
2010-12-31 11:31:57 -0500

    Don't use httparty for get_titles, move to static method.

--- lib/ruby-redbox.rb
+++ lib/ruby-redbox.rb
@@ -2,15 +2,21 @@ require 'rubygems'
 gem 'httparty'
 require 'httparty'
 
-class Redbox
+class RedboxAPI
   include HTTParty
   base_uri "www.redbox.com"
   
-  def initialize
-  end
+  class << self
   
-  def get_titles
-    response = self.class.get("/api/product/js/titles")[13..-1]    
-    @titles ||= response
-  end  
+    def get_titles
+      url = URI.parse('http://www.redbox.com/api/product/js/titles')
+      response = Net::HTTP.start(url.host, url.port){|http| http.get(url.path)}
+      case response
+      when Net::HTTPSuccess, Net::HTTPRedirection
+        @titles = response.body[13..-1]
+      else
+        response.error
+      end
+    end  
+  end
 end
