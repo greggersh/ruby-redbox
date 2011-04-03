50d4dd6e5e002a686c0584f4feea06a3dfa95fd5
Greg Gershman <greggersh@yahoo.com>
2011-04-02 21:33:54 -0400

    Modify parsing of Redbox response; returns string with message on success=false.

--- lib/ruby-redbox.rb
+++ lib/ruby-redbox.rb
@@ -20,7 +20,7 @@ class RedboxAPI
     
     def find_nearest_kiosks(title_id, lat, lon)
       body = "{\"filters\":{\"proximity\":{\"lat\":#{lat},\"lng\":#{lon},\"radius\":50}},\"resultOptions\":{\"max\":50,\"profile\":true,\"status\":true,\"proximity\":true,\"user\":true,\"inventory\":true,\"inventoryProducts\":[\"#{title_id}\"]}}"
-      headers = {"Host" => "www.redbox.com", "Accept" => "application/json, text/javascript, */*", "X-Requested-With" => "XMLHttpRequest", "User-Agent" => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13", "Cookie" => "ASP.NET_SessionId=przaowkuetjyjsezsu3cqooe; rbuser=Validation=exXiQC/PjMAZY2Z3jWqNpaaSrje+peGrA5EcJ7rvuY4=; LB=2021814026.0.0000; RB_2.1=1; s_cc=true; c_m=undefinedDirect%20LoadDirect%20Load; s_sq=%5B%5BB%5D%5D; s_vi=[CS]v1|26CB2B70851D3189-60000128600DAA0B[CE]; _json=%7B%22locationSearchValue%22%3A%22%5C%2221209%5C%22%22%7D", "__K" => "zI9Y1ZyWISIwgrAhIfSZElRqonnXBG12UqNNl1HCRFA="}
+      headers = {"Host" => "www.redbox.com", "Accept" => "application/json, text/javascript, */*", "X-Requested-With" => "XMLHttpRequest", "User-Agent" => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13", "Cookie" => "rbuser=Validation=exXiQC/PjMAZY2Z3jWqNpaaSrje+peGrA5EcJ7rvuY4=; RB_2.1=1;", "__K" => "zI9Y1ZyWISIwgrAhIfSZElRqonnXBG12UqNNl1HCRFA="}
       url = URI.parse("http://www.redbox.com/api/Store/GetStores/")
       request = Net::HTTP::Post.new(url.path)
       request.body = body
@@ -28,7 +28,12 @@ class RedboxAPI
         request[key] = value
       end
       response = Net::HTTP.new(url.host, url.port).start {|http| http.request(request) }
-      NearestKiosks.new(JSON.parse(response.body))
+      parsed_response = JSON.parse(response.body)
+      if parsed_response["d"]["success"] == false
+        return parsed_response["d"]["msg"]
+      else
+        return []
+      end
     end
   end
   
